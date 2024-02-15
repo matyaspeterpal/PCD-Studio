4be84425bd3adbf43b12dd641c1129f62e7e642b
matyaspeterpal <matyaspeterpal@freemail.hu>
2024-02-15 20:02:35 +0100

    Initial commit

---
+++ Widgets/pcdStudio.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+def test():
+
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+    widget.resize(400, 200)
+    widget.setWindowTitle("This is PyQt Widget example")
+    widget.show()
+    exit(app.exec_())
